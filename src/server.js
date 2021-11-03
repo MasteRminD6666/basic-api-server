15ee7781d46deb2d7d7ab8573e5a63b73d9d2f5f
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-03 15:56:40 +0200

    editting

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@
 const express = require("express");
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3030
 const logger = require('./middleware/logger')
 
 const notFound = require('./error-handlers/404')
